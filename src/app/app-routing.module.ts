87dbc59eabe3623274025190131f4c44142822b8
Brian Gunn <github@gunnbr.org>
2017-10-31 20:58:44 -0700

    Added skeleton stats and admin pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './home/welcome.component';
 import { FactoidListComponent } from './factoids/factoid-list.component';
+import { AdminComponent } from './admin/admin.component';
+import { StatsComponent } from './stats/stats.component';
 
 const routes: Routes = [
   {path: 'factoids', component: FactoidListComponent},
   {path: 'welcome', component: WelcomeComponent},
+  {path: 'stats', component: StatsComponent},
+  {path: 'admin', component: AdminComponent},
   {path: '', redirectTo: 'welcome', pathMatch: 'full'},
   {path: '**', redirectTo: 'welcome', pathMatch: 'full'}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,16 @@ import { WelcomeComponent } from './home/welcome.component';
 import { FactoidListComponent } from './factoids/factoid-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { StatsComponent } from './stats/stats.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
     FactoidListComponent,
+    StatsComponent,
+    AdminComponent,
   ],
   imports: [
     BrowserModule,
