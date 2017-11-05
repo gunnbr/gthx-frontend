5ae9b7d1e19e289a674e0a0cfe9c47d01cb60b92
Brian Gunn <github@gunnbr.org>
2017-11-04 21:41:39 -0700

    Disabled the auth header for queries to the old server.

--- src/app/authinterceptor.service.ts
+++ src/app/authinterceptor.service.ts
@@ -8,6 +8,13 @@ export class AuthInterceptorService implements HttpInterceptor{
     constructor(private injector: Injector) {}
 
     intercept(req, next) {
+        // TODO: Remove this once everything is 
+        //       runnig from a single server that
+        //       supports the authorization
+        if (req.url.indexOf('amazonaws') != -1){
+            return next.handle(req)
+        }
+        
         var auth = this.injector.get(AuthService)
         var authRequest = req.clone({
             headers: req.headers.set('Authorization', 'token ' + auth.token)
